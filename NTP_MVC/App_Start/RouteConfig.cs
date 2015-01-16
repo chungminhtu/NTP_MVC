3f034c7759bba7e7df4f0d2afb71b21495e7f22c
Your Name <you@example.com>
2015-01-15 16:35:04 -0800

    Sửa Tree Menu OK

--- NTP_MVC/App_Start/RouteConfig.cs
+++ NTP_MVC/App_Start/RouteConfig.cs
@@ -14,7 +14,7 @@ namespace NTP_MVC {
             routes.MapRoute(
                 name: "Default", // Route name
                 url: "{controller}/{action}/{id}", // URL with parameters
-                defaults: new { controller = "SoKhamBenh", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
         }
     }
