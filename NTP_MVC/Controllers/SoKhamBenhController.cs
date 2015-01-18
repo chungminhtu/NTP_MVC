70c99bec66afe496cbd0afbd5f289da7b6620d1f
Your Name <you@example.com>
2015-01-18 08:22:52 -0800

    Hoàn thành Phần search BenhNhan và SoKhamBenh của bệnh nhân đó (có thể edit- addnew)

--- NTP_MVC/Controllers/SoKhamBenhController.cs
+++ NTP_MVC/Controllers/SoKhamBenhController.cs
@@ -21,9 +21,28 @@ namespace NTP_MVC.Controllers
         [ValidateInput(false)]
         public ActionResult SoKhamBenhGridViewPartial()
         {
-            var model =   db.SO_SoKhamBenh;
-            return PartialView("_SoKhamBenhGridViewPartial",  model.Take(3000).ToList());
-        } 
+            ViewData["ListPXNBenhNhan"] = null;
+            var model = db.SO_SoKhamBenh.Take(0).ToList();
+            return PartialView("_SoKhamBenhGridViewPartial", model);
+        }
+
+        public ActionResult GetDetailBenhNhan()
+        {
+            var s = Request.Params["ID_BenhNhan"] + "";
+            var model = (SO_BenhNhan)(from bn in db.SO_BenhNhan
+                                      where bn.SoCMND.Contains(s) || bn.MaBNQL.Contains(s) || bn.HoTen.Contains(s) || bn.ID_BenhNhan.ToString().Contains(s)
+                                      select bn).FirstOrDefault();
+            //ViewData["IDBenhNhan"] = model.ID_BenhNhan;
+            return PartialView("ThongTinBenhNhan", model);
+        }
+         
+        public ActionResult GetListSoKhamBenh()
+        {
+            long IDBenhNhan = Convert.ToInt64(Request.Params["ID_BenhNhan"].ToString().Split(',')[0]);
+            var model = db.SO_SoKhamBenh.Where(bn => bn.ID_BENHNHAN == IDBenhNhan).ToList();
+            ViewData["IDBenhNhan"] = IDBenhNhan;
+            return PartialView("_SoKhamBenhGridViewPartial", model);
+        }
 
         [HttpPost, ValidateInput(false)]
         public ActionResult SoKhamBenhGridViewPartialAddNew(SO_SoKhamBenh item)
@@ -89,6 +108,77 @@ namespace NTP_MVC.Controllers
             }
             return PartialView("_SoKhamBenhGridViewPartial", model.ToList());
         }
-     
+
+        [ValidateInput(false)]
+        public ActionResult PhieuXetNghiemGridPartial()
+        {
+            ViewData["ListPXNBenhNhan"] = db.SO_PhieuXetNghiem.Take(8).ToList();
+            return PartialView("_PhieuXetNghiemGridPartial", ViewData["ListPXNBenhNhan"]);
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult PhieuXetNghiemGridPartialAddNew(NTP_MVC.Models.SO_PhieuXetNghiem item)
+        {
+            var model = db.SO_PhieuXetNghiem;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Add(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_PhieuXetNghiemGridPartial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult PhieuXetNghiemGridPartialUpdate(NTP_MVC.Models.SO_PhieuXetNghiem item)
+        {
+            var model = db.SO_PhieuXetNghiem;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var modelItem = model.FirstOrDefault(it => it.ID_Phieuxetnghiem == item.ID_Phieuxetnghiem);
+                    if (modelItem != null)
+                    {
+                        this.UpdateModel(modelItem);
+                        db.SaveChanges();
+                    }
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_PhieuXetNghiemGridPartial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult PhieuXetNghiemGridPartialDelete(System.Int64 ID_Phieuxetnghiem)
+        {
+            var model = db.SO_PhieuXetNghiem;
+            if (ID_Phieuxetnghiem >= 0)
+            {
+                try
+                {
+                    var item = model.FirstOrDefault(it => it.ID_Phieuxetnghiem == ID_Phieuxetnghiem);
+                    if (item != null)
+                        model.Remove(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            return PartialView("_PhieuXetNghiemGridPartial", model.ToList());
+        }
     }
 }
