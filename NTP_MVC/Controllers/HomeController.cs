ca315080d25fb61d558b54a5898af8b6c3947fd2
Minh Tú <chungminhtu@gmail.com>
2015-01-22 00:24:01 +0700

    Sua xong form Benh Nhan External Edit-Addnew và Sửa xong Popup SoKhamBenh

--- NTP_MVC/Controllers/HomeController.cs
+++ NTP_MVC/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,18 +12,25 @@ namespace NTP_MVC.Controllers
         NTP_DBEntities db = new NTP_DBEntities();
         public ActionResult Index()
         {
-            return View();    
+            return View();
         }
         [HttpPost]
         public ActionResult Login(AD_Users user)
         {
-            //if (db.AD_Users.Where(u=>u.Username.Equals(user.Username)).Where(u=>u.Password.Equals(user.Password)).Any() )
-            //{
-            //    return View("/SoKhamBenh/Index.cshtml");
-            //}
-            //return View();
-            return RedirectToAction("Index","SoKhamBenh");
+            var User = (AD_Users)db.AD_Users.Where(u => u.Username.Equals(user.Username)).Where(u => u.Password.Equals(user.Password)).SingleOrDefault();
+            if (User != null)
+            {
+                ControllerContext.HttpContext.Session["MATINH"] = User.MA_TINH + "";
+                ControllerContext.HttpContext.Session["MAHUYEN"] = User.MA_HUYEN + "";
+
+                return RedirectToAction("Index", "BenhNhan"); 
+            }
+            else
+            {
+                ViewData["EditError"] = "Tên đăng nhập hoặc mật khẩu không đúng! Hãy thử lại.";
+                return RedirectToAction("Index", "Home");
+            }
         }
-    
+
     }
 }
