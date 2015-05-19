c3b512fc00e1eb7aa5c629aa97c005929a8cfc54
chungminhtu <chungminhtu@gmail.com>
2015-05-19 14:50:34 +0700

    sua bug

---
+++ NTP_MVC/Reports/PhieuXetNghiem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace NTP_MVC.Reports
+{
+    public partial class PhieuXetNghiem : DevExpress.XtraReports.UI.XtraReport
+    {
+        public PhieuXetNghiem()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
